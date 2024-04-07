39a04e449b540cd04dd451123b6fbeb26291f1f1
boomsi <libo19431@gmail.com>
2024-04-07 13:47:42 +0800

    update

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -18,3 +18,35 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('POST', 'GET'))
+def create():
+    if request.method == 'POST':
+        author_id = g.user['id']
+        title = request.form['title']
+        body = request.form['body']
+
+        if not author_id:
+            return redirect(url_for('auth.login'))
+
+        error = None
+        if not title:
+            error = 'Title is required.'
+        if not body:
+            error = 'Body is required.'
+        
+        if not error:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES(?, ?, ?)',
+                (title, body, author_id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+        flash(error)
+    return render_template('blog/create.html')
+
+@bp.route('/update', methods=('GET', 'POST'))
+def update():
+    pass
